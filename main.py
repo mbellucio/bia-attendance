a62e550931274de989488689dc9fa35a82c0d288
Matheus <=>
2023-04-11 10:03:00 -0300

    update attendance so far, april.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from playtime_calculator import PlaytimeCalculator
 
 
 #===============================
-FILENAME = 'BIA_WW2_2023_03_26'
-MONTH = 'march'
+FILENAME = 'BIA_WW2_2023_04_09'
+MONTH = 'april'
 #===============================
 
 
