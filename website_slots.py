37dcaba8142f4bf8e9142224621d2532a631ded8
Matheus <=>
2023-04-20 14:17:12 -0300

    add website slots 04/20/2023

--- website_slots.py
+++ website_slots.py
@@ -8,7 +8,7 @@ chrome_driver_path = r"chromedriver.exe"
 s = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=s)
 
-mission_link = 'https://biaarma.com/event/OpRetribution'
+mission_link = 'https://biaarma.com/event/OpBlastsInBroadDaylight'
 
 op_name = mission_link.split('/')
 op_name = op_name[4]
