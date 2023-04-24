f5cf17a4958f23273db58b49a58729297e2e71b3
Matheus <=>
2023-04-24 08:47:30 -0300

    fix training att params

--- playtime_calculator.py
+++ playtime_calculator.py
@@ -28,7 +28,7 @@ class PlaytimeCalculator:
 
             type_test = self.file_name.split("_")
             if type_test[1] == 'TRN':
-                self.minimum_playtime = 60
+                self.minimum_playtime = 40
 
             if playtime_in_minutes > self.minimum_playtime:
                 self.attended_list.append(name)
