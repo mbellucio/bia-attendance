9813910b3d0e577ee1093a0f66f18383e811f380
Matheus <=>
2023-05-11 14:50:34 -0300

    website so far

--- website_slots.py
+++ website_slots.py
@@ -8,7 +8,7 @@ chrome_driver_path = r"chromedriver.exe"
 s = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=s)
 
-mission_link = 'https://biaarma.com/event/OpSidiBarrani'
+mission_link = 'https://biaarma.com/event/OpMaloyRaid'
 
 op_name = mission_link.split('/')
 op_name = op_name[4]
