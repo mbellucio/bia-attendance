cc784d489ec1bd55ca9f9122f34ee67b7bd78e0c
Matheus <=>
2023-06-18 18:54:21 -0300

    june attendance so far

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from playtime_calculator import PlaytimeCalculator
 
 
 #===============================
-FILENAME = 'BIA_TRN_2023_06_06'
+FILENAME = 'BIA_MOD_2023_06_15'
 MONTH = 'june'
-MISSION_DURATION = 129; 
+MISSION_DURATION = 163; 
 #===============================
 
 
