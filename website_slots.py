e60c39cce5d9919ad0a5b55219a5c8db1d71428e
Matheus <=>
2023-07-20 14:23:27 -0300

    July Website slots so far

--- website_slots.py
+++ website_slots.py
@@ -8,7 +8,7 @@ chrome_driver_path = r"chromedriver.exe"
 s = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=s)
 
-mission_link = 'https://biaarma.com/event/operation-barbarossa-campaignbattle-of-bialystok-37ac'
+mission_link = 'https://biaarma.com/event/OpUrgentFury'
 
 op_name = mission_link.split('/')
 op_name = op_name[4]
