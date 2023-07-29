39965728ae2b77fef5f48aff040a9a71a2a42f86
Matheus <=>
2023-07-29 14:16:04 -0300

    july attendance so far

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from log_processor import LogProcessor
 from data_structurer import DataStructurer
 from playtime_calculator import PlaytimeCalculator
+from mission_duration.duration import get_mission_duration
 
-
-#===============================
-FILENAME = 'BIA_MOD_2023_07_06'
+# ===============================
+FILENAME = 'BIA_TRN_2023_07_25'
 MONTH = 'july'
-MISSION_DURATION = 153; 
-#===============================
+# ===============================
 
+try: 
+  mission_duration = get_mission_duration(month=MONTH, mission=FILENAME)
+except KeyError:
+  mission_duration = None
 
 log_processor = LogProcessor(file_name=FILENAME, month=MONTH)
 log_data = log_processor.process_log()
 
-data_structurer = DataStructurer(processed_log=log_data, file_name=FILENAME, month=MONTH)
+data_structurer = DataStructurer(
+    processed_log=log_data, file_name=FILENAME, month=MONTH)
 structured_data = data_structurer.organize_by_player()
 
-playtime_calculator = PlaytimeCalculator(structured_data=structured_data, filename=FILENAME, month=MONTH, mission_duration=MISSION_DURATION)
+playtime_calculator = PlaytimeCalculator(
+    structured_data=structured_data,
+    filename=FILENAME, month=MONTH,
+    mission_duration=mission_duration
+)
 playtime_calculator.final_attendance()
-
---
+++ mission_duration/duration.py
@@ -0,0 +1,6 @@
+import json
+
+def get_mission_duration(month:str, mission:str):
+    f = open(f'mission_duration/{month}.json')
+    data = json.load(f)
+    return data[mission]["Duration"]
