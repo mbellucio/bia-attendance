412c693314a2f93146342d9c400ed830fe61617e
mbellucio <=>
2023-10-29 19:06:29 -0300

    finish website slots october

--- website_slots.py
+++ website_slots.py
@@ -8,7 +8,7 @@ chrome_driver_path = r"chromedriver.exe"
 s = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=s)
 
-mission_link = 'https://biaarma.com/event/2nd-battle-of-el-alamein-campaignoperation-supercharge-912e'
+mission_link = 'https://biaarma.com/event/OpBattleBryansk'
 
 op_name = mission_link.split('/')
 op_name = op_name[4]
