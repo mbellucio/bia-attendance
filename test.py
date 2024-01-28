e33a23c6f30e6418eae178c159bba80ffa8fa66c
mbellucio <=>
2024-01-28 15:38:02 -0300

    january website slots

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-def fizzBuzz(n):
-    for i in range(1, n + 1):
-      if i % 3 == 0 and i % 5 == 0:
-          print('FizzBuzz')
-      elif i % 3 == 0:
-          print('Fizz')
-      elif i % 5 == 0:
-          print('Buzz')
-      else: 
-         print(i)
+def ehprimo(num):
+  divisores = 0
+  i = 1
+ 
+  if num % 2 == 0:
+    divisores = 2
 
-
-fizzBuzz(15)
+  if divisores == 2:
+    return True
+  else:
+    return False
+  
+print(ehprimo(13))
