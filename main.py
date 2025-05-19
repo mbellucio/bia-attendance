fea6c2b20d793720ea55ba13a2104caf4d093c70
mbellucio <matheusbellucio@gmail.com>
2025-05-18 22:08:01 -0300

    May attendance so far

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from mission_duration.duration import get_mission_duration
 import os
 
 # =========================
-MONTH = 'april'
+MONTH = 'may'
 YEAR = '2025'
 ROOT_DIR = 'logs'
 # =========================
