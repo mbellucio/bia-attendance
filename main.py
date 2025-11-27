63da34be8d4dcbd4f8c607c387a73a4ab490f8de
mbellucio <matheusbellucio@gmail.com>
2025-11-27 20:18:04 -0300

    November attendance so far

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from mission_duration.duration import get_mission_duration
 import os
 
 # =========================
-MONTH = 'october'
+MONTH = 'november'
 YEAR = '2025'
 ROOT_DIR = 'logs'
 # =========================
